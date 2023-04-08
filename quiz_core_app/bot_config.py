77299fe86bc165a8ac2047e209fbf0dc745e8242
Aleksey Petravets <112317442+Pelex2Up@users.noreply.github.com>
2023-04-08 17:27:13 +0300

    Update bot_config.py

--- quiz_core_app/bot_config.py
+++ quiz_core_app/bot_config.py
@@ -1,4 +1,4 @@
 import telebot
 
-admin_chat_id = '-1001577146751'
-bot = telebot.TeleBot('6052324876:AAG4UZ9crDk_ryqCud9e2mooeMNKPRxnGYE')
+admin_chat_id = '-{chat_id}'
+bot = telebot.TeleBot('TOKEN')
